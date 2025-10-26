3a2c548b170ccb319e6d1b0749ce8486a9ade95f
Moonwalker <moonwalker1092@gmail.com>
2025-10-26 22:01:04 +0530

    Update FORMATS.py

--- plugins/FORMATS.py
+++ plugins/FORMATS.py
@@ -2,10 +2,10 @@
 
 #text formatting....
 
-on_pic = "https://envs.sh/VxL.jpg"
-off_pic = "https://envs.sh/Vx5.jpg"
-files_cmd_pic = "https://envs.sh/Vx_.jpg"
-autodel_cmd_pic = "https://envs.sh/Vxc.jpg"
+on_pic = "https://envs.sh/JHA.jpg"
+off_pic = "https://envs.sh/JH5.jpg"
+files_cmd_pic = "https://envs.sh/JHG.jpg"
+autodel_cmd_pic = "https://envs.sh/JHZ.jpg"
 
 #start message
 START_MSG = """<b>⚡ Hᴇʏ, {mention} ~
