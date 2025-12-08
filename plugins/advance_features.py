4e0364ef4783d2e48400c501d18e9f8f6caa2e15
{remo} <moonwalker1092@gmail.com>
2025-12-08 16:13:36 +0530

    Add error handling and force sub mode command

--- plugins/advance_features.py
+++ plugins/advance_features.py
@@ -421,3 +421,36 @@ async def files_commands(client: Client, message: Message):
                 link = link
             ),
             reply_markup = InlineKeyboardMarkup(
+                [InlineKeyboardButton(f'Pʀᴏᴛᴇᴄᴛ Cᴏɴᴛᴇɴᴛ: {pcd}', callback_data='pc'), InlineKeyboardButton(f'Hɪᴅᴇ Cᴀᴘᴛɪᴏɴ: {hcd}', callback_data='hc')],
+                [InlineKeyboardButton(f'Cʜᴀɴɴᴇʟ Bᴜᴛᴛᴏɴ: {cbd}', callback_data='cb'), InlineKeyboardButton(f'◈ Sᴇᴛ Bᴜᴛᴛᴏɴ ➪', callback_data='setcb')],
+                [InlineKeyboardButton('🔄 Rᴇғʀᴇsʜ', callback_data='files_cmd'), InlineKeyboardButton('Cʟᴏsᴇ ✖️', callback_data='close')]
+            ]),
+            message_effect_id = 5107584321108051014 #👍
+        )
+    except Exception as e:
+        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Cʟᴏsᴇ ✖️", callback_data = "close")]])
+        await message.reply(f"<b>! Eʀʀᴏʀ Oᴄᴄᴜʀᴇᴅ..\n<blockquote>Rᴇᴀsᴏɴ:</b> {e}</blockquote><b><i>Cᴏɴᴛᴀɴᴄᴛ ᴅᴇᴠᴇʟᴏᴘᴇʀ: @metaui</i></b>", reply_markup=reply_markup)
+
+
+#Request force sub mode commad,,,,,,
+@Bot.on_message(filters.command('req_fsub') & filters.private & ~banUser)
+async def handle_reqFsub(client: Client, message: Message):
+    await message.reply_chat_action(ChatAction.TYPING)
+    try:
+        on = off = ""
+        if await kingdb.get_request_forcesub():
+            on = "🟢"
+            texting = on_txt
+        else:
+            off = "🔴"
+            texting = off_txt
+
+        button = [
+            [InlineKeyboardButton(f"{on} ON", "chng_req"), InlineKeyboardButton(f"{off} OFF", "chng_req")],
+            [InlineKeyboardButton("⚙️ Mᴏʀᴇ Sᴇᴛᴛɪɴɢs ⚙️", "more_settings")]
+        ]
+        await message.reply(text=RFSUB_CMD_TXT.format(req_mode=texting), reply_markup=InlineKeyboardMarkup(button), message_effect_id=5046509860389126442)
+        
+    except Exception as e:
+        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Cʟᴏsᴇ ✖️", callback_data = "close")]])
+        await message.reply(f"<b>! Eʀʀᴏʀ Oᴄᴄᴜʀᴇᴅ..\n<blockquote>Rᴇᴀsᴏɴ:</b> {e}</blockquote><b><i>Cᴏɴᴛᴀɴᴄᴛ ᴅᴇᴠᴇʟᴏᴘᴇʀ: @Here_remo</i></b>", reply_markup=reply_markup)
