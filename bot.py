99015869ce7931672208b8f1b13fd3e9db73dfc4
{remo} <moonwalker1092@gmail.com>
2025-12-09 18:23:10 +0530

    Refactor bot restart message handling

--- bot.py
+++ bot.py
@@ -61,13 +61,18 @@ await runner.setup()
 bind_address = "0.0.0.0"
 await web.TCPSite(runner, bind_address, PORT).start()
 
-        try:
-    await self.send_message(
-        OWNER_ID,
-        text="<b><blockquote>🤖 Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ ♻️</blockquote></b>"
-    )
-except:
-    pass
+async def start(self):
+    await super().start()
+    
+    # ... other startup code ...
+
+    try:
+        await self.send_message(
+            OWNER_ID,
+            text="<b><blockquote>🤖 Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ ♻️</blockquote></b>"
+        )
+    except:
+        pass
 
     async def stop(self, *args):
         await super().stop()
