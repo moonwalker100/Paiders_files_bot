5c6f1d54685fcce8b599af1306536a179b6ad700
{remo} <moonwalker1092@gmail.com>
2025-12-09 14:13:17 +0530

    Rename start_web_server to web_server and set max size

--- plugins/web_server.py
+++ plugins/web_server.py
@@ -1,7 +1,7 @@
 from aiohttp import web
 from .route import routes
 
-async def start_web_server():
-    app = web.Application()
+async def web_server():
+    app = web.Application(client_max_size=30000000)
     app.add_routes(routes)
     return app
